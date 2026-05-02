48abf61e9b78c317f512a0f06a3877dd2d11024e
Samuel Flores <123712593+Rojoosam@users.noreply.github.com>
2026-05-02 00:39:07 -0600

    Fix CORS header quotes in Program.cs

--- Program.cs
+++ Program.cs
@@ -127,7 +127,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseCors(“AllowFrontend”);
+app.UseCors("AllowFrontend");
 //app.UseHttpsRedirection();
 
 app.UseAuthentication();
